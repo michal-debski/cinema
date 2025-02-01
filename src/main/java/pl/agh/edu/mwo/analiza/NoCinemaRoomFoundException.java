f63121293610c69177417c63df1ca78ed11da12d
Michal Debski <michal.d9777@gmail.com>
2025-02-01 23:35:31 +0100

    add necessary exception when cinema room not found

---
+++ src/main/java/pl/agh/edu/mwo/analiza/NoCinemaRoomFoundException.java
@@ -0,0 +1,7 @@
+package pl.agh.edu.mwo.analiza;
+
+public class NoCinemaRoomFoundException extends RuntimeException {
+    public NoCinemaRoomFoundException(String message) {
+        super(message);
+    }
+}
